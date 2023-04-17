6a66dc2d8872e3a43fe09df6e2ae0cef931360a3
Dominic Fuentes <militaryforce1@yahoo.com>
2023-04-17 18:13:41 -0500

    clean up warnings

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,10 @@
 import { VscGithub } from 'react-icons/vsc';
 import { SlSocialLinkedin } from 'react-icons/sl';
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { SlArrowDown } from 'react-icons/sl';
 
 const Header = ({ onNavClick }) => {
 
-  const [showMenu, setShowMenu] = useState(false);
-
-  const handleMenuClick = () => {
-    setShowMenu(!setShowMenu);
-  }
   return (
     <div className="header">
       <div className="header-div">
@@ -26,13 +20,13 @@ const Header = ({ onNavClick }) => {
         </div>
         <div className="slogan">
           <h3>
-            Letting ideas come to life, one line of code at a time.
+            Transforming ideas into reality, one line of code at a time.
           </h3>
         </div>
         <div className='header-links'>
-          <a className='header-linkdin-icon' href='https://www.linkedin.com/in/dominicfuentes1/' target="_blank"><SlSocialLinkedin />
+          <a className='header-linkdin-icon' href='https://www.linkedin.com/in/dominicfuentes1/' target="_blank" rel='noreferrer'><SlSocialLinkedin />
           </a>
-          <a className='header-github-icon' href='https://github.com/fuentesdominic' target="_blank"><VscGithub />
+          <a className='header-github-icon' href='https://github.com/fuentesdominic' target="_blank" rel='noreferrer'><VscGithub />
           </a>
         </div>
         <div className='header-image-div'>
