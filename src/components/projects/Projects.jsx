c34895822ebbd5744b367fe3ddd62d93ada45cfc
Dominic Fuentes <militaryforce1@yahoo.com>
2023-04-18 18:19:48 -0500

    giving info to projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,6 +1,79 @@
-const Projects = () => {
+import './Projects.css';
+import { VscGithub } from 'react-icons/vsc';
+import { BiPlayCircle } from 'react-icons/bi';
+
+const Projects = ({id}) => {
+
+  const projects = [
+    {
+      projectName: 'Brunch',
+      img: 'https://i.imgur.com/OxjWHZH.png',
+      technologyUsed: 'JavaScript | PostgreSQL | Express | React | Node',
+      description: 'This application has User Authentication and shows various restuarnats throughout Lake Ozark, Missouri.',
+      links: {
+        github: 'https://github.com/fuentesdominic/brunch',
+        live: 'https://brunch-peach.vercel.app/register'
+      }
+    },
+    {
+      projectName: 'CodeUp',
+      img: 'https://i.imgur.com/FtMX9m2.png',
+      technologyUsed: 'JavaScript | PostgreSQL | Express | React | Node',
+      description: 'FIrst project using user authentication. This application can be used to help beginners learn code.',
+      links: {
+        github: 'https://github.com/fuentesdominic/CodeUp',
+        live: 'https://code-up.vercel.app/register'
+      }
+    },
+    {
+      projectName: 'NFL List',
+      img: 'https://i.imgur.com/TcFh2AX.png',
+      technologyUsed: 'JavaScript | MongoDB | Express | React | Node',
+      description: 'Encourages users to navigate the divisions and coferences of the NFL to view information about teams.',
+      links: {
+        github: 'https://github.com/fuentesdominic/NFL_List',
+        live: 'https://nfl-list.herokuapp.com/'
+      }
+    },
+    {
+      projectName: 'Coding Coasters',
+      img: 'https://i.imgur.com/v5sydFw.png',
+      technologyUsed: 'JavaScript | MongoDB | Express | React | Node',
+      description: 'First project collaborating with peers. This application allows users to create theme parks and add rides.',
+      links: {
+        github: 'https://github.com/noahdvaughn/hackathon-project',
+        live: 'https://hackathon-dom-josh-noah.herokuapp.com/'
+      }
+    },
+    {
+      projectName: 'Find Four',
+      img: 'https://i.imgur.com/zvnPh8J.png',
+      technologyUsed: 'JavaScript | HTML | CSS',
+      description: 'Two player game that alternates players until one achieves a line of four connected circles.',
+      links: {
+        github: 'https://github.com/fuentesdominic/connect_four_project',
+        live: 'https://find-four-connect-4.surge.sh/'
+      }
+    }
+  ]
+
   return (
-    <div>Projects</div>
+    <div className='projects-page' id={id}>
+      <div className='projects-page-title'>
+        <h3>Projects</h3>
+      </div>
+      <div className='projects'>
+        {projects.map((project) => (
+          <div className='project-div'>
+            <img src={project.img} alt='' />
+            <h2>{project.projectName}</h2>
+            <h3>{project.technologyUsed}</h3>
+            <p>{project.description}</p>
+            <p>Links: <a href={project.links.github}>Github<VscGithub /></a> && <a href={project.links.live}>Live<BiPlayCircle /> </a> </p>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 export default Projects
