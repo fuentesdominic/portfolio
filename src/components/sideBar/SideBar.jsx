19727aac293ce9494dabde5c316a4900756d3821
Dominic Fuentes <militaryforce1@yahoo.com>
2023-04-18 19:06:31 -0500

    add navigating to items

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -1,6 +1,37 @@
-const SideBar = () => {
+import './SideBar.css';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const SideBar = ({ onNavClick }) => {
+
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleMenuClick = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <div>SideBar</div>
+    <nav className='nav'>
+      <div className='nav-items'>
+        <ul className={`items ${showMenu ? 'show' : ''}`}>
+          <li className='item'>
+            <Link onClick={() => onNavClick('header')}>Home</Link>
+          </li>
+          <li className='item'>
+            <Link onClick={() => onNavClick('about')}>About</Link>
+          </li>
+          <li className='item'>
+            <Link onClick={() => onNavClick('projects')}>Projects</Link>
+          </li>
+          <li className='item'>
+            <Link onClick={() => onNavClick('contact')}>Contact</Link>
+          </li>
+          <li className='item'>
+            <a href='https://docs.google.com/document/d/13EZzbupTfKS3CSzTydAIo1M4spQxeOgnOvWgXOXrKig/edit?usp=sharing' target='_blank' rel='noreferrer'>Resume</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 export default SideBar
